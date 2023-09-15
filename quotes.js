4b7ccf5c7660d4094759267dfd00f4af6f90834e
Vivian Bow <vivianbao1009@gmail.com>
2023-09-15 21:37:39 +0800

    Set up GET /api/quotes route

--- quotes.js
+++ quotes.js
@@ -1,19 +1,27 @@
 const express = require('express');
+const { quotes } = require('./data');
 
 const quotesRouter = express.Router();
 
-// get all quotes
-quotes.get('/', (req, res)=>{
-
+// get all quotes (if there's query, fetch by person)
+quotesRouter.get('/', (req, res)=>{
+  let targetQuotesArr = [];
+  const queryExists = Object.keys(req.query).length > 0
+  if(queryExists){
+    const person = req.query.person;
+    quotes.forEach((obj)=>{
+      if(obj.person === person){
+        targetQuotesArr.push(obj);
+      }
+    })
+  }else{
+    quotes.forEach((obj)=>{
+      targetQuotesArr.push(obj);
+    })
+  }
+  const response = {quotes: targetQuotesArr};
+  res.send(response);
 })
 
-// get quotes by author
-quotes.get('/', (req, res)=>{
 
-})
-
-
-// get random quotes
-quotes.get('/', (req, res)=>{
-
-})
+module.exports = quotesRouter;
